242f58664fa12f507e361f48de24522d79eeba7d
Taufik Nurrohman <hi@taufik-nurrohman.com>
2022-04-04 04:40:01 +0700

    Update

--- search/index.php
+++ search/index.php
@@ -122,7 +122,7 @@ if (0 !== $key && null !== ($query = \get($_GET, $key))) {
                     ]
                 ]);
                 $GLOBALS['t'][] = \i('Error');
-                return \Y::error($path . '/' . ($i + 1), [], 404);
+                return ['error', [], 404];
             }
             // Apply the hook only if there is a match
             \Hook::set([
@@ -145,7 +145,7 @@ if (0 !== $key && null !== ($query = \get($_GET, $key))) {
             ]);
             $GLOBALS['t'][] = i('Search');
             $GLOBALS['t'][] = '&#x201C;' . $r['query'] . '&#x201D;';
-            return \Y::pages($path . '/' . ($i + 1), [], 200);
+            return ['pages', [], 200];
         }, 100);
     } else {
         \Hook::set('route.search', function($content, $path, $query, $hash, $r) {
@@ -164,7 +164,7 @@ if (0 !== $key && null !== ($query = \get($_GET, $key))) {
                 ]
             ]);
             $GLOBALS['t'][] = \i('Error');
-            return \Y::error($path, [], 404);
+            return ['error', [], 404];
         }, 100);
     }
     if ("" !== ($q = (string) $query)) {
@@ -175,4 +175,4 @@ if (0 !== $key && null !== ($query = \get($_GET, $key))) {
     }
 }
 
-!\Y::path('form/search') && \Y::set('form/search', __DIR__ . \D . 'lot' . \D . 'y' . \D . 'form' . \D . 'search.php');
+!\Layout::path('form/search') && \Layout::set('form/search', __DIR__ . \D . 'engine' . \D . 'y' . \D . 'form' . \D . 'search.php');
