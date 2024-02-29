6c73005e182aa60027b66a2c6089903d9fa11f17
Taufik Nurrohman <hi@taufik-nurrohman.com>
2024-02-29 09:58:29 +0700

    File-Less Layout

--- index.php
+++ index.php
@@ -77,8 +77,7 @@ function route__page($content, $path, $query, $hash) {
         if (!$page->exist) {
             return $content;
         }
-        $t = $GLOBALS['t'] ?? [];
-        if ($t && $t instanceof \Anemone && $t->count > 1)  {
+        if (isset($t) && $t instanceof \Anemone && $t->count > 1)  {
             if (\i('Error') === $t->last) {
                 $GLOBALS['t']->last(true); // Remove “Error” title from the previous `route` hook(s) if any
             }
@@ -186,8 +185,58 @@ function route__search($content, $path, $query, $hash) {
     }
 }
 
-if (\class_exists("\\Layout")) {
-    !\Layout::path('form/search') && \Layout::set('form/search', __DIR__ . \D . 'engine' . \D . 'y' . \D . 'form' . \D . 'search.php');
+if (\class_exists("\\Layout") && !\Layout::of('form/search')) {
+    \Layout::set('form/search', static function (string $key, array $lot = []) {
+        \extract($lot, \EXTR_SKIP);
+        \extract($GLOBALS, \EXTR_SKIP);
+        $key = $lot['key'] ?? null;
+        $has_key = isset($key) && \is_string($key);
+        $has_path = !empty($url->path);
+        $has_route = isset($route) && \is_string($route);
+        $is_page = $site->is('page');
+        $current = $has_path ? $url->current(false, false) : null;
+        $key = $has_key ? $key : ($state->x->search->key ?? null);
+        $title = $is_page ? ($page->parent ? ($page->parent->title ?? null) : null) : ($page->title ?? null);
+        return new \HTML(\Hook::fire('y.form.search', [[
+            0 => 'form',
+            1 => [
+                'search' => [
+                    0 => 'p',
+                    1 => [
+                        'input' => [
+                            0 => 'input',
+                            1 => false,
+                            2 => [
+                                'name' => null !== $key ? (false !== \strpos(\strtr($key, ["\\." => \P]), '.') ? (static function ($key) {
+                                    // Convert `foo.bar.baz` to `foo[bar][baz]`
+                                    $keys = \explode('.', \strtr($key, ["\\." => \P]));
+                                    $v = \strtr(\array_shift($keys), [\P => '.']);
+                                    while (\is_string($key = \array_shift($keys))) {
+                                        $v .= '[' . \strtr($key, [\P => '.']) . ']';
+                                    }
+                                    return $v;
+                                })($key) : \strtr($key, ["\\." => '.'])) : null,
+                                'placeholder' => $title && !$has_route ? \i('Search in %s', ['&#x201c;' . \S . $title . \S . '&#x201d;']) : null,
+                                'type' => 'text',
+                                'value' => null !== $key ? \get($_GET, $key) : null
+                            ]
+                        ],
+                        ' ' => ' ',
+                        'button' => [
+                            0 => 'button',
+                            1 => \i('Search'),
+                            2 => ['type' => 'submit']
+                        ]
+                    ]
+                ]
+            ],
+            2 => [
+                'action' => $has_route ? ($url . '/' . \trim($route, '/') . '/1') : ($has_path && $is_page ? \dirname($current) . '/1' : null),
+                'method' => 'get',
+                'name' => 'search'
+            ]
+        ]], $page), true);
+    });
 }
 
 \Hook::set('route.page', __NAMESPACE__ . "\\route__page", 100.1);
